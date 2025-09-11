ece4bd2c28318b1cf8cba09ef785b91389bdffa9
Liviu2911 <pirvanliviu29@gmail.com>
2025-09-11 13:50:45 +0300

    Create account page created

--- client/src/components/navbar/menu/index.tsx
+++ client/src/components/navbar/menu/index.tsx
@@ -7,7 +7,7 @@ function Menu() {
       <Navlink position={1600}>Pricing</Navlink>
       <Navlink position={2400}>Features</Navlink>
       <Link to="/login" className="opacity-80 hover:opacity-100 px-3 py-1.5 rounded transition-all hover:bg-stone-800 outline-none hover:text-primary">Log In</Link>
-      <Link to="/" className="transition-all text-stone-200 cursor-pointer bg-primary/80 px-3 ml-2 py-1.5 rounded hover:bg-primary/90">
+      <Link to="/register" className="transition-all text-stone-200 cursor-pointer bg-primary/80 px-3 ml-2 py-1.5 rounded hover:bg-primary/90">
         Create Account
       </Link>
     </div>
--- client/src/routes/register/index.tsx
+++ client/src/routes/register/index.tsx
@@ -1,9 +1,31 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import Form from "../../components/form";
+import Input from "../../components/form/input";
+import Button from "../../components/form/button";
 
-export const Route = createFileRoute('/register/')({
+export const Route = createFileRoute("/register/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/register/"!</div>
+  return (
+    <div className="flex flex-col gap-8 items-center">
+      <div className="flex flex-col gap-2 items-center mt-24 mb-6">
+        <h1 className="font-bold sm:text-4xl">Create Account</h1>
+        <Link
+          to="/login"
+          className="text-primary opacity-80 hover:opacity-100 transition-all"
+        >
+         Log In
+        </Link>
+      </div>
+      <Form onSubmit={() => {}}>
+        <Input name="username" />
+        <Input name="email" />
+        <Input name="password" password />
+        <Input name="repassword" password />
+        <Button text="Create Account" />
+      </Form>
+    </div>
+  );
 }
